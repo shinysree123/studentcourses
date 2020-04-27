0fea8cf40487507682f7aa27350ff9194a605255
shinysree123 <shinyjoseph89@gmail.com>
2020-04-27 13:53:19 +0530

    erorr corrected

--- models/studentmodel.js
+++ models/studentmodel.js
@@ -1,11 +1,14 @@
 var Mongoose=require('mongoose');
 const studentSchema= new Mongoose.Schema(
-    {name:String,
+    {
+    name:String,
     emailid:String,
     place:String,
     phone:Number,
     parentname:String,
-    parentphone:Number}
+    parentphone:Number
+
+}
 );
 var studentModel=Mongoose.model('students',studentSchema);
 module.exports={studentModel}
--- student.js
+++ student.js
@@ -4,10 +4,10 @@ var Mongoose=require('mongoose');
 var app=Express();
 var Studentrouter=require('./router/studentrouter');
 var Courserouter=require('./router/courserouter');
-app.use(Parser.urlencoded({extended:false}))
-Mongoose.connect("mongodb+srv://shinyjoseph:shiny@cluster0-aebge.mongodb.net/test?retryWrites=true&w=majority")
+app.use(Parser.urlencoded({extended:false}));
+Mongoose.connect("mongodb+srv://shinyjoseph:shiny@cluster0-aebge.mongodb.net/test?retryWrites=true&w=majority");
 app.use('/',Studentrouter);
 app.use('/course',Courserouter);
 app.listen(process.env.PORT||4000,()=>{
     console.log("server staredd");
-})
+});
